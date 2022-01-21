b17b8cebb6afd24174fc1cfaf9c7b92820973fa6
m.heider <michael.heider@informatik.uni-augsburg.de>
2022-01-21 15:26:41 +0100

    Fix an issue with a nix syntax change

--- slurm/default.nix
+++ slurm/default.nix
@@ -1,12 +1,10 @@
 with import (builtins.fetchGit {
-  name = "nixpkgs-2021-12-08";
   url = "https://github.com/NixOS/nixpkgs/";
   rev = "35ad3c79b6c264aa73bd8e7ca1dd0ffb67bd73b1";
 }) { config = { allowUnfree = true; }; };
 
 
 mkShell {
-  name = "beste-shell";
   venvDir = "./_venv";
   # Add dependencies that pip can't fetch here (or that we don't want to
   # install using pip).
