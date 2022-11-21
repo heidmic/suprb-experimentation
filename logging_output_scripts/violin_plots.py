bfb13fb54c3f834ec491e994b9ac0429a8d88936
Roman Sraj <Roman.Sraj@outlook.com>
2022-11-21 15:50:13 +0100

    Update sbatch; Update Readme; Adjust logging scripts

---
+++ logging_output_scripts/violin_plots.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from mlflow_utils import get_dataframe
+
+"""
+Uses seaborn-package to create violin-Plots comparing model performances
+on multiple datasets
+"""
+sns.set_style("whitegrid")
+
+heur = ['ES', 'RS', 'NS', 'MCNS', 'NSLC']
+
+datasets = ["concrete_strength", 'combined_cycle_power_plant',
+            'airfoil_self_noise', 'energy_cool']
+
+
+dict_list = {}
+
+for problem in datasets:
+    res_var = 0
+    first = True
+    for directory in heur:
+        fold_df = get_dataframe(directory, problem)
+        if not fold_df.empty:
+            name = []
+            for x in range(fold_df.shape[0]):
+                name.append(directory)
+            # Adds additional column for plotting
+            if first:
+                res_var = fold_df.assign(Used_Representation=name)
+                first = False
+            else:
+                res_var = pd.concat([res_var, fold_df.assign(Used_Representation=name)])
+
+            print(f"Done for {problem} with {directory}")
+
+    # Invert values since they are stored as negatives
+    res_var['metrics.test_neg_mean_squared_error'] = -res_var['metrics.test_neg_mean_squared_error']
+    # Store violin-plots of all models in one plot
+    fig, ax = plt.subplots()
+    ax = sns.violinplot(x='Used_Representation', y='metrics.test_neg_mean_squared_error', data=res_var)
+    ax.set(xlabel='RD method')
+    ax.set(ylabel='MSE')
+
+    # TODO Change directory
+    output_dir = "Violins"
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    fig.savefig(f"{output_dir}/{problem}.png")
