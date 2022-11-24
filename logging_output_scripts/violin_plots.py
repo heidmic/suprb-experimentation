0a4938f0a187e0c5d013b58b540561a992d49162
Roman Sraj <Roman.Sraj@outlook.com>
2022-11-24 12:20:13 +0100

    Add run_all_scripts; Add option to use mlflow or csv

--- logging_output_scripts/violin_plots.py
+++ logging_output_scripts/violin_plots.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
-from mlflow_utils import get_dataframe
+from logging_output_scripts.utils import get_dataframe, create_output_dir, config
+
 
 """
 Uses seaborn-package to create violin-Plots comparing model performances
@@ -10,43 +10,45 @@ on multiple datasets
 """
 sns.set_style("whitegrid")
 
-heuristics = ['ES', 'RS', 'NS', 'MCNS', 'NSLC']
+final_output_dir = f"{config['output_directory']}/violin_plots"
+create_output_dir(config['output_directory'])
+create_output_dir(final_output_dir)
 
-datasets = ["concrete_strength", 'combined_cycle_power_plant',
-            'airfoil_self_noise', 'energy_cool']
+if config['filetype'] == 'csv':
+    test_neg_mean_squared_error = "test_neg_mean_squared_error"
+elif config['filetype'] == 'mlflow':
+    test_neg_mean_squared_error = "metrics.test_neg_mean_squared_error"
 
 
-dict_list = {}
+def create_violin_plots():
+    for problem in config['datasets']:
+        res_var = 0
+        first = True
+        for heuristic in config['heuristics']:
+            fold_df = get_dataframe(heuristic, problem)
+            if fold_df is not None:
+                name = []
+                for x in range(fold_df.shape[0]):
+                    name.append(heuristic)
+                # Adds additional column for plotting
+                if first:
+                    res_var = fold_df.assign(Used_Representation=name)
+                    first = False
+                else:
+                    res_var = pd.concat([res_var, fold_df.assign(Used_Representation=name)])
 
-for problem in datasets:
-    res_var = 0
-    first = True
-    for heuristic in heuristics:
-        fold_df = get_dataframe(heuristic, problem)
-        if not fold_df.empty:
-            name = []
-            for x in range(fold_df.shape[0]):
-                name.append(heuristic)
-            # Adds additional column for plotting
-            if first:
-                res_var = fold_df.assign(Used_Representation=name)
-                first = False
-            else:
-                res_var = pd.concat([res_var, fold_df.assign(Used_Representation=name)])
+                print(f"Done for {problem} with {heuristic}")
 
-            print(f"Done for {problem} with {heuristic}")
+        # Invert values since they are stored as negatives
+        res_var[test_neg_mean_squared_error] = -res_var[test_neg_mean_squared_error]
+        # Store violin-plots of all models in one plot
+        fig, ax = plt.subplots()
+        ax = sns.violinplot(x='Used_Representation', y=test_neg_mean_squared_error, data=res_var)
+        ax.set(xlabel='RD method')
+        ax.set(ylabel='MSE')
 
-    # Invert values since they are stored as negatives
-    res_var['metrics.test_neg_mean_squared_error'] = -res_var['metrics.test_neg_mean_squared_error']
-    # Store violin-plots of all models in one plot
-    fig, ax = plt.subplots()
-    ax = sns.violinplot(x='Used_Representation', y='metrics.test_neg_mean_squared_error', data=res_var)
-    ax.set(xlabel='RD method')
-    ax.set(ylabel='MSE')
+        fig.savefig(f"{final_output_dir}/{problem}.png")
 
-    # TODO Change directory
-    output_dir = "Violins"
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
 
-    fig.savefig(f"{output_dir}/{problem}.png")
+if __name__ == '__main__':
+    create_violin_plots()
