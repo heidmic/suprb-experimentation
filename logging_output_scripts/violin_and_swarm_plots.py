23a14955388309730fd55fc99a84dca98cd479be
Roman Sraj <Roman.Sraj@outlook.com>
2023-06-28 11:28:30 +0200

    Cleanup violing and swarm plots

--- logging_output_scripts/violin_and_swarm_plots.py
+++ logging_output_scripts/violin_and_swarm_plots.py
@@ -1,10 +1,8 @@
-from logging_output_scripts.utils import check_and_create_dir, get_dataframe, get_all_runs, get_df
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import json
-from sklearn.preprocessing import MinMaxScaler
+from logging_output_scripts.utils import get_dataframe, create_output_dir, config
 
 
 """
@@ -26,31 +24,19 @@ plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 plt.tight_layout()
 
 
-mse = "metrics.test_neg_mean_squared_error"
-# mse = "metrics.elitist_complexity"
+final_output_dir = f"{config['output_directory']}"
+create_output_dir(config['output_directory'])
+create_output_dir(final_output_dir)
+metric = "metrics.test_neg_mean_squared_error"
 
 
 def create_plots():
-    with open('logging_output_scripts/config.json') as f:
-        config = json.load(f)
-
-    final_output_dir = f"{config['output_directory']}"
-
-    for output_dir in ["violin_plots", "swarm_plots", "line_plots"]:
-        check_and_create_dir(final_output_dir, output_dir)
-
-    final_output_dir = f"{config['output_directory']}"
-    scaler = MinMaxScaler()
-    
     for problem in config['datasets']:
         first = True
-        res_var = 0
-        counter = 0
-        for heuristic, renamed_heuristic in config['heuristics'].items():
-            fold_df = get_df(heuristic, problem)
+        for heuristic in config['heuristics']:
+            fold_df = get_dataframe(heuristic['name'], problem)
             if fold_df is not None:
-                counter += 1
-                name = [renamed_heuristic] * fold_df.shape[0]
+                name = [heuristic['rename']] * fold_df.shape[0]
                 current_res = fold_df.assign(Used_Representation=name)
                 if first:
                     first = False
@@ -59,65 +45,38 @@ def create_plots():
                     # Adds additional column for plotting
                     res_var = pd.concat([res_var, current_res])
 
-                print(f"Done for {problem} with {renamed_heuristic}")
+                print(f"Done for {problem} with {heuristic}")
 
-        if counter and config["normalize_datasets"]:
-            reshaped_var = np.array(res_var[mse])[-counter*64:].reshape(counter, -1) * -1
-            scaler.fit(reshaped_var)
-            scaled_var = scaler.transform(reshaped_var)
-            scaled_var = scaled_var.reshape(1, -1)[0]
-            res_var[mse][-len(scaled_var):] = scaled_var
+    # Invert values since they are stored as negatives
+    res_var[metric] *= -1
 
-        # Invert values since they are stored as negatives
-        if not config["normalize_datasets"]:
-            res_var[mse] *= -1
+    def ax_config(axis):
+        ax.set_xlabel('Estimator', weight="bold")
+        ax.set_ylabel('MSE', weight="bold")
+        title_dict = {"concrete_strength": "Concrete Strength",
+                      "combined_cycle_power_plant": "Combined Cycle Power Plant",
+                      "airfoil_self_noise": "Airfoil Self Noise",
+                      "energy_cool": "Energy Efficiency Cooling"}
+        ax.set_title(title_dict[problem], style="italic")
+        ax.set_box_aspect(1)
 
-        def ax_config(axis):
-            ax.set_xlabel('Estimator', weight="bold")
-            ax.set_ylabel('MSE', weight="bold")
-            ax.set_title(config['datasets'][problem] if not config["normalize_datasets"]
-                        else "Normalized Datasets", style="italic")
-            ax.set_box_aspect(1)
+    # Store violin-plots of all models in one plot
+    fig, ax = plt.subplots()
+    ax = sns.violinplot(x='Used_Representation', y=metric, data=res_var, scale="width", scale_hue=False)
+    ax_config(ax)
+    fig.savefig(f"{final_output_dir}/violin_plots/{problem}.png")
 
-        problem = problem if not config["normalize_datasets"] else "normalized"
-
-        # Store violin-plots of all models in one plot
-        fig, ax = plt.subplots()
-        ax = sns.violinplot(x='Used_Representation', y=mse, data=res_var, scale="width", scale_hue=False)
-        ax_config(ax)
-        fig.savefig(f"{final_output_dir}/violin_plots/{problem}.png")
-
-        # Store swarm-plots of all models in one plot
-        fig, ax = plt.subplots()
-        order = np.sort(res_var['Used_Representation'].unique())
-        ax = sns.swarmplot(x='Used_Representation', y=mse, data=res_var, size=4)
-        # ax = sns.pointplot(x='Used_Representation', y=mse, order=order,
-        #                    data=res_var, ci=None, color='black')
-        ax_config(ax)
-        fig.savefig(f"{final_output_dir}/swarm_plots/{problem}.png", dpi=500)
-
-        # Store line-box-plots
-        fig, ax = plt.subplots()
-
-        order = np.sort(res_var['Used_Representation'].unique())
-        ax = sns.boxplot(x='Used_Representation', y=mse, order=order,
-                        showfliers=True, linewidth=0.8, showmeans=True, data=res_var)
-        ax = sns.pointplot(x='Used_Representation', y=mse, order=order,
-                        data=res_var, ci=None, color='black')
-        ax_config(ax)
-        fig.savefig(f"{final_output_dir}/line_plots/{problem}.png")
-
-        # Store line-box-plots
-        fig, ax = plt.subplots()
-
-        order = np.sort(res_var['Used_Representation'].unique())
-        ax = sns.boxplot(x='Used_Representation', y=mse, order=order,
-                        showfliers=True, linewidth=0.8, showmeans=True, data=res_var)
-        ax = sns.pointplot(x='Used_Representation', y=mse, order=order,
-                        data=res_var, ci=None, color='black')
-        ax_config(ax)
-        fig.savefig(f"{final_output_dir}/line_plots/{problem}.png")
+    # Store swarm-plots of all models in one plot
+    fig, ax = plt.subplots()
+    ax = sns.swarmplot(x='Used_Representation', y=metric, data=res_var, size=2)
+    ax_config(ax)
+    fig.savefig(f"{final_output_dir}/swarm_plots/{problem}.png", dpi=500)
 
 
 if __name__ == '__main__':
+    for dir in ["", "violin_plots", "swarm_plots"]:
+        directory = f"{final_output_dir}/{dir}"
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+
     create_plots()
