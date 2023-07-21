0fbd4dfc269fec9fe0a2c3f76818d02ae4a7dc2a
RomanSraj <roman.sraj@outlook.com>
2023-07-21 11:50:35 +0200

    Adjust slurm scripts

--- slurm/multiple.sh
+++ slurm/multiple.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-datasets=("concrete_strength" "energy_cool" "combined_cycle_power_plant" "airfoil_self_noise")
-scripts=("runs/comparisons/rf.py")
+datasets=("concrete_strength") #"energy_cool" "combined_cycle_power_plant" "airfoil_self_noise" "LEV", "german.numer" "laser" "fourclass_scale" "machine_cpu" "boston" "meta" "chscase_foot")
+
+scripts=("runs/rule_discovery/evolution_strategy.py runs/rule_discovery/random_search.py")
+# scripts=("runs/rule_discovery/novelty_search.py")
+
+ns_types=("NS" "MCNS" "NSLC")
 
 for script in ${scripts[@]}; do
     for dataset in ${datasets[@]}; do
+		export dataset=$dataset
+		export experiment=$script
+
+		# for ns in ${ns_types[@]}; do
+		# 	export ns_type=$ns
+		# 	export archive_pop=true
+		# 	sbatch slurm/rule_discovery.sbatch
+
+		# 	sleep 5
+		# 	export archive_pop=false
+		# 	sbatch slurm/rule_discovery.sbatch
+
+
     	echo $script $dataset
-	export dataset=$dataset
-	export experiment=$script
-	sbatch slurm/default.sbatch
+
+	
+	
+	sbatch slurm/rule_discovery.sbatch
     done
 done
 
