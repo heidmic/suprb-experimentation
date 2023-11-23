813ff35a0795745b6d1476f0379ac1cef3fef33b
Thomas Niemeier <thomasniemeier99@gmail.com>
2023-11-23 20:18:54 +0100

    bugfix

--- logging_output_scripts/run_all_scripts.py
+++ logging_output_scripts/run_all_scripts.py
@@ -1,10 +1,10 @@
-from logging_output_scripts.violin_plots import create_violin_plots
+from logging_output_scripts.violin_and_swarm_plots import create_plots
 from logging_output_scripts.calc_bay import calc_bayes
 from logging_output_scripts.summary_csv import create_summary_csv
 from logging_output_scripts.latex_tabulars import write_complexity, write_mse, single_table
 
 if __name__ == '__main__':
-    create_violin_plots()
+    create_plots()
     calc_bayes()
     try:
         create_summary_csv()
