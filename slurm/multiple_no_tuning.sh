04f2cb77c32d0d7ff4f50f541815406fa2505008
RomanSraj <roman.sraj@outlook.com>
2024-09-03 09:11:46 +0200

    Fix sbatch typo

--- slurm/multiple_no_tuning.sh
+++ slurm/multiple_no_tuning.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-filenames = ("basic" "rf" decision_tree)
+filenames = ("basic" "rf" "decision_tree")
 for fname in ${filenames[@]}; do 
 	export filename=$fname
 		sbatch slurm/basic.sbatch
