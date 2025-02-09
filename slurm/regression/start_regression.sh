a61bd5751c7f42f5acdb59e3a08824502f37c9f3
wehrfabi <fabian.wehr@uni-a.de>
2025-02-09 12:30:03 +0100

    slurm_regression

---
+++ slurm/regression/start_regression.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+JOB_DIR=/data/oc-compute03/wehrfabi/my-experimentation
+MODELS=("lasso" "ridge" "elasticNet")
+PROBLEMS=("concrete_strength" "airfoil_self_noise" "combined_cycle_power_plant" "energy_heat")
+
+for model in "${MODELS[@]}"; do
+    for problem in "${PROBLEMS[@]}"; do
+        sbatch $JOB_DIR/slurm/regression/regress_tuning_${model}_${problem}.sh
+    done
+done
