900b481917c5896c8a84ff07b09246dc999511d6
RomanSraj <roman.sraj@outlook.com>
2025-04-08 13:03:26 +0200

    Use python 3.10

--- slurm/default.nix
+++ slurm/default.nix
@@ -8,12 +8,12 @@ mkShell {
   venvDir = "./_venv";
   # Add dependencies that pip can't fetch here (or that we don't want to
   # install using pip).
-  buildInputs = (with pkgs.python39Packages; [ python39 venvShellHook wheel ])
+  buildInputs = (with pkgs.python310Packages; [ python310 venvShellHook wheel ])
      ++ (import ./system-dependencies.nix { inherit pkgs; });
   postShellHook = ''
     unset SOURCE_DATE_EPOCH
     export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc]}:${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH"
-    export PYTHONPATH=$venvDir/${python39.sitePackages}:$PYTHONPATH
+    export PYTHONPATH=$venvDir/${python310.sitePackages}:$PYTHONPATH
   '';
   postVenvCreation = ''
     unset SOURCE_DATE_EPOCH
