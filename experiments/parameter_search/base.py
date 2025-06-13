dba0968af33641ba56e83f68989a99f98c6cd734
RomanSraj <roman.sraj@outlook.com>
2025-06-13 14:26:47 +0200

    Use latest sklearn and add respective requirements

--- experiments/parameter_search/base.py
+++ experiments/parameter_search/base.py
@@ -8,10 +8,11 @@ from sklearn.model_selection import cross_validate
 from sklearn.utils import Bunch
 
 from . import metrics
+from sklearn.metrics import get_scorer_names
 
 
 def _validate_sklearn_metric(metric: str) -> bool:
-    return metric in sklearn.metrics.SCORERS.keys()
+    return metric in get_scorer_names()
 
 
 def _validate_own_metric(metric: str) -> bool:
@@ -24,18 +25,19 @@ class ParameterTuner(metaclass=ABCMeta):
     tuning_result_: Bunch
     tuned_params_: dict
 
-    def __init__(self,
-                 estimator: BaseEstimator,
-                 X_train: np.ndarray,
-                 y_train: np.ndarray,
-                 scoring: Union[str, Callable],
-                 n_calls: int = 32,
-                 cv: int = None,
-                 n_jobs_cv: int = None,
-                 n_jobs: int = None,
-                 verbose: int = 0,
-                 random_state: int = None
-                 ):
+    def __init__(
+        self,
+        estimator: BaseEstimator,
+        X_train: np.ndarray,
+        y_train: np.ndarray,
+        scoring: Union[str, Callable],
+        n_calls: int = 32,
+        cv: int = None,
+        n_jobs_cv: int = None,
+        n_jobs: int = None,
+        verbose: int = 0,
+        random_state: int = None,
+    ):
         self.estimator = estimator
         self.X_train = X_train
         self.y_train = y_train
@@ -51,7 +53,7 @@ class ParameterTuner(metaclass=ABCMeta):
         """The default objective function performs cross-validation and uses the average of the scoring as value."""
 
         estimator = clone(self.estimator)
-        initial_params = {'random_state': self.random_state} | initial_params
+        initial_params = {"random_state": self.random_state} | initial_params
 
         def objective(**params):
             estimator.set_params(**(initial_params | params))
@@ -64,15 +66,15 @@ class ParameterTuner(metaclass=ABCMeta):
                 n_jobs=self.n_jobs_cv,
                 return_estimator=True,
                 verbose=self.verbose,
-                error_score='raise',
+                error_score="raise",
             )
 
             if _validate_sklearn_metric(self.scoring):
-                score = scores['test_score']
+                score = scores["test_score"]
             elif _validate_own_metric(self.scoring):
-                score = [getattr(metrics, self.scoring)(_estimator) for _estimator in scores['estimator']]
+                score = [getattr(metrics, self.scoring)(_estimator) for _estimator in scores["estimator"]]
             else:
-                raise ValueError('invalid scoring metric')
+                raise ValueError("invalid scoring metric")
 
             return -np.mean(score)
 
@@ -82,7 +84,7 @@ class ParameterTuner(metaclass=ABCMeta):
         return {key: getattr(self, key) for key in keys}
 
     def get_params(self):
-        return self._get_params(['scoring', 'n_calls', 'cv', 'random_state'])
+        return self._get_params(["scoring", "n_calls", "cv", "random_state"])
 
     @abstractmethod
     def __call__(self, parameter_space: dict[str, Any], local_params: dict) -> tuple[dict, Any]:
--- runs/comparisons/suprb_tuning.py
+++ runs/comparisons/suprb_tuning.py
@@ -21,7 +21,6 @@ from suprb.logging.stdout import StdoutLogger
 from suprb.optimizer.solution import ga
 from suprb.optimizer.rule import es, origin, mutation
 from suprb.solution.initialization import RandomInit
-import suprb.solution.mixing_model as mixing_model
 
 
 random_state = 42
@@ -30,33 +29,38 @@ random_state = 42
 def load_dataset(name: str, **kwargs) -> tuple[np.ndarray, np.ndarray]:
     method_name = f"load_{name}"
     from problems import datasets
+
     if hasattr(datasets, method_name):
         return getattr(datasets, method_name)(**kwargs)
 
 
 @click.command()
-@click.option('-p', '--problem', type=click.STRING, default='airfoil_self_noise')
-@click.option('-j', '--job_id', type=click.STRING, default='NA')
-@click.option('-r', '--rule_amount', type=click.INT, default=1)
-@click.option('-f', '--filter_subpopulation', type=click.STRING, default='FilterSubpopulation')
-@click.option('-e', '--experience_calculation', type=click.STRING, default='ExperienceCalculation')
-@click.option('-w', '--experience_weight', type=click.INT, default=1)
-def run(problem: str, job_id: str, rule_amount: int, filter_subpopulation: str,
-        experience_calculation: str, experience_weight: int):
+@click.option("-p", "--problem", type=click.STRING, default="airfoil_self_noise")
+@click.option("-j", "--job_id", type=click.STRING, default="NA")
+@click.option("-r", "--rule_amount", type=click.INT, default=1)
+@click.option("-f", "--filter_subpopulation", type=click.STRING, default="FilterSubpopulation")
+@click.option("-e", "--experience_calculation", type=click.STRING, default="ExperienceCalculation")
+@click.option("-w", "--experience_weight", type=click.INT, default=1)
+def run(
+    problem: str,
+    job_id: str,
+    rule_amount: int,
+    filter_subpopulation: str,
+    experience_calculation: str,
+    experience_weight: int,
+):
     print(f"Problem is {problem}, with job id {job_id}")
 
     X, y = load_dataset(name=problem, return_X_y=True)
-    X, y = scale_X_y(X, y)
+    X, y, _ = scale_X_y(X, y)
     X, y = shuffle(X, y, random_state=random_state)
 
     estimator = SupRB(
         rule_discovery=es.ES1xLambda(
-            operator='&',
+            operator="&",
             n_iter=1000,
             delay=30,
-            init=rule.initialization.MeanInit(fitness=rule.fitness.VolumeWu(),
-                                              model=Ridge(alpha=0.01,
-                                                          random_state=random_state)),
+            init=rule.initialization.MeanInit(fitness=rule.fitness.VolumeWu(), model=Ridge(alpha=0.01, random_state=random_state)),
             mutation=mutation.HalfnormIncrease(),
             origin_generation=origin.SquaredError(),
         ),
@@ -64,8 +68,7 @@ def run(problem: str, job_id: str, rule_amount: int, filter_subpopulation: str,
         n_iter=32,
         n_rules=4,
         verbose=10,
-        logger=CombinedLogger(
-            [('stdout', StdoutLogger()), ('default', DefaultLogger())]),
+        logger=CombinedLogger([("stdout", StdoutLogger()), ("default", DefaultLogger())]),
     )
 
     tuning_params = dict(
@@ -75,9 +78,9 @@ def run(problem: str, job_id: str, rule_amount: int, filter_subpopulation: str,
         n_jobs_cv=4,
         n_jobs=4,
         n_calls=1000,
-        timeout=60*60*24,  # 24 hours
-        scoring='neg_mean_squared_error',
-        verbose=10
+        timeout=60 * 60 * 24,  # 24 hours
+        scoring="neg_mean_squared_error",
+        verbose=10,
     )
 
     @param_space()
@@ -85,37 +88,23 @@ def run(problem: str, job_id: str, rule_amount: int, filter_subpopulation: str,
         # ES
         sigma_space = [0, np.sqrt(X.shape[1])]
 
-        params.rule_discovery__mutation__sigma = trial.suggest_float('rule_discovery__mutation__sigma', *sigma_space)
-        params.rule_discovery__init__fitness__alpha = trial.suggest_float(
-            'rule_discovery__init__fitness__alpha', 0.01, 0.2)
+        params.rule_discovery__mutation__sigma = trial.suggest_float("rule_discovery__mutation__sigma", *sigma_space)
+        params.rule_discovery__init__fitness__alpha = trial.suggest_float("rule_discovery__init__fitness__alpha", 0.01, 0.2)
 
         # GA
-        params.solution_composition__selection__k = trial.suggest_int('solution_composition__selection__k', 3, 10)
+        params.solution_composition__selection__k = trial.suggest_int("solution_composition__selection__k", 3, 10)
 
-        params.solution_composition__crossover = trial.suggest_categorical(
-            'solution_composition__crossover', ['NPoint', 'Uniform'])
+        params.solution_composition__crossover = trial.suggest_categorical("solution_composition__crossover", ["NPoint", "Uniform"])
         params.solution_composition__crossover = getattr(ga.crossover, params.solution_composition__crossover)()
 
         if isinstance(params.solution_composition__crossover, ga.crossover.NPoint):
-            params.solution_composition__crossover__n = trial.suggest_int('solution_composition__crossover__n', 1, 10)
+            params.solution_composition__crossover__n = trial.suggest_int("solution_composition__crossover__n", 1, 10)
 
-        params.solution_composition__mutation__mutation_rate = trial.suggest_float(
-            'solution_composition__mutation_rate', 0, 0.1)
+        params.solution_composition__mutation_rate = trial.suggest_float("solution_composition__mutation_rate", 0.0, 0.1)
 
-        # Upper and lower bound clip the experience into a given range
-        # params.solution_composition__init__mixing__experience_calculation__lower_bound = trial.suggest_float(
-        #     'solution_composition__init__mixing__experience_calculation__lower_bound', 0, 10)
-
-        if isinstance(params.solution_composition__init__mixing__experience_calculation, mixing_model.CapExperienceWithDimensionality):
-            params.solution_composition__init__mixing__experience_calculation__upper_bound = trial.suggest_float(
-                'solution_composition__init__mixing__experience_calculation__upper_bound', 2, 5)
-        else:
-            params.solution_composition__init__mixing__experience_calculation__upper_bound = trial.suggest_int(
-                'solution_composition__init__mixing__experience_calculation__upper_bound', 20, 50)
-
-    experiment_name = f'SupRB Tuning j:{job_id} p:{problem}; r:{rule_amount}; f:{filter_subpopulation}; -e:{experience_calculation}'
+    experiment_name = f"SupRB Tuning j:{job_id} p:{problem}; r:{rule_amount}; f:{filter_subpopulation}; -e:{experience_calculation}"
     print(experiment_name)
-    experiment = Experiment(name=experiment_name,  verbose=10)
+    experiment = Experiment(name=experiment_name, verbose=10)
 
     tuner = OptunaTuner(X_train=X, y_train=y, **tuning_params)
     experiment.with_tuning(suprb_ES_GA_space, tuner=tuner)
@@ -123,15 +112,13 @@ def run(problem: str, job_id: str, rule_amount: int, filter_subpopulation: str,
     random_states = np.random.SeedSequence(random_state).generate_state(8)
     experiment.with_random_states(random_states, n_jobs=8)
 
-    evaluation = CrossValidate(
-        estimator=estimator, X=X, y=y, random_state=random_state, verbose=10)
+    evaluation = CrossValidate(estimator=estimator, X=X, y=y, random_state=random_state, verbose=10)
 
-    experiment.perform(evaluation, cv=ShuffleSplit(
-        n_splits=8, test_size=0.25, random_state=random_state), n_jobs=8)
+    experiment.perform(evaluation, cv=ShuffleSplit(n_splits=8, test_size=0.25, random_state=random_state), n_jobs=8)
 
     mlflow.set_experiment(experiment_name)
     log_experiment(experiment)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
