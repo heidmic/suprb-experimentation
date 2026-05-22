e2a913c76473a02ed266e7f8efcdaec1914a0ec9
wolfbene <benedikt.wolf@uni-a.de>
2026-05-22 20:24:34 +0200

    slurm update

--- slurm/devenv.nix
+++ slurm/devenv.nix
@@ -10,32 +10,36 @@
     gccStdenv.cc.cc.lib
     libz
   ];
+
   languages = {
     python = {
       # If you don't need Python, comment this out:
-        enable = true;
+      enable = true;
 
       # Choose your Python version:
-        package = pkgs.python312;
-        version = "3.12.11";  # Use this only if you need a specific patch version, may build from source
+      package = pkgs.python312;
+      version = "3.12.11"; # Use this only if you need a specific patch version, may build from source
 
       # Use the uv package manager:
-        uv = {
-            enable = true;
-            sync.enable = true;
-        };
+      uv = {
+        enable = true;
+        sync.enable = true;
+      };
 
       # Use venv and requirements.txt:
-        venv = {
-          enable = true;
-          requirements = ./requirements.txt;  # Create this yourself
-        };
+      venv = {
+        enable = true;
+        requirements = ./requirements.txt; # Create this yourself
+      };
     };
 
-    languages.rust = {
+    # HIER KORRIGIERT: Da wir schon "inside" von languages sind, 
+    # reicht hier einfach nur "rust =" statt "languages.rust ="
+    rust = {
       # If you need Rust, comment this in:
       #   enable = true;
       #   channel = "stable"; # or "nixpkgs" or "nightly"
       #   version = "1.81.0"; # or "latest"
     };
   };
+}
