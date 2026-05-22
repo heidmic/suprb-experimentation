d27e60890d87803c594ebde8e957700ddb384438
wolfbene <benedikt.wolf@uni-a.de>
2026-05-22 15:10:48 +0200

    slurm update

--- slurm/flake.nix
+++ slurm/flake.nix
@@ -1,15 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
+    
+    # Diese beiden Zeilen lösen den Fehler:
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nixpkgs-python.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, devenv, ... }@inputs:
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       devShells.${system}.default = devenv.lib.mkShell {
+        # Hier wird "inputs" übergeben, wodurch devenv jetzt Zugriff auf nixpkgs-python hat
         inherit inputs pkgs;
         modules = [ (import ./devenv.nix) ];
       };
