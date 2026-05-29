a114381689eda8c450ab5ed49d48366cae37951b
wolfbene <benedikt.wolf@uni-a.de>
2026-05-29 20:06:01 +0200

    fixes and adding for testing

--- runs/moo_sc/two_stage_comparison.py
+++ runs/moo_sc/two_stage_comparison.py
@@ -55,7 +55,7 @@ def run(problem: str, job_id: str, optimizer: str, config: str, early_stopping: 
     print(f"Problem is {problem}, with job id {job_id} and optimizer {optimizer}")
 
     X, y = load_dataset(name=problem, return_X_y=True)
-    X, y = scale_X_y(X, y)
+    X, y, _ = scale_X_y(X, y)
     X, y = shuffle(X, y, random_state=random_state)
 
     sc_iter = 1000 if early_stopping else 32
